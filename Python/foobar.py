33ac6d8e6e7551e7a40f3117afbe81bb310eaf10
kpkovale <kpkovale@gmail.com>
2022-03-28 19:12:34 +1000

    python foobar script added

---
+++ Python/foobar.py
@@ -0,0 +1,18 @@
+def output_val(number):
+    if number % 15 == 0:
+        return "foobar"
+    elif i % 5 == 0:
+        return "bar"
+    elif i % 3 == 0:
+        return "foo"
+    else:
+        return number
+
+for i in range(1,101):
+    if i != 100:
+        delimiter = ', '
+        ending = ''
+    else:
+        delimiter = ''
+        ending = '\n'
+    print(output_val(i),delimiter,sep='',end=ending)
