2bc7ba20fcffc2a787e67db0ced5d9669193f23d
kpkovale <kpkovale@gmail.com>
2022-03-30 03:26:32 +1000

    file processing finalized

--- Python/user_upload.py
+++ Python/user_upload.py
@@ -1,13 +1,13 @@
-import getopt
 import sys
-import argparse
 from user_defined_functions import *
 import csv
 import os.path
+import re
+from email.utils import parseaddr
 
 # Collecting the list of arguments
 arguments = get_CLI_options(sys.argv[1:])
-print(arguments)
+# print(arguments)
 
 if arguments.dry_run is not None:
     isDryRun = True
@@ -20,10 +20,38 @@ NEED_FILE_MESSAGE = """Script defined exception: File path or file name is requi
 Please restart the script using correct parameters"""
 
 if arguments.create_table is None:
-    print("no create_table option. starting file processing")
+    print("--create_table option was not given. Starting the file processing")
     if arguments.file is None:
         sys.exit(NEED_FILE_MESSAGE)
     elif not os.path.exists(arguments.file):
         sys.exit("File " + arguments.file + " not found. Try using another file name")
     else:
-        print(os.path.abspath(arguments.file))
+        with open(arguments.file, mode='r') as csvFile:
+            resFile = []
+            fileReader = csv.DictReader(csvFile,
+                                        skipinitialspace=True,
+                                        quoting=csv.QUOTE_NONE,
+                                        fieldnames=["name", "surname", "email"])
+            for row in fileReader:
+                resFile.append(row)
+
+        # check for the header row
+        if (resFile[0]["name"] == "name") & (resFile[0]["surname"] == "surname"):
+            resFile = resFile[1:]
+
+        # Processing names and surnames using regexp
+        pattern = re.compile("[A-z]+(\'|-)[A-z]+|[A-z]+")
+        for row in resFile:
+            row["name"] = format_complex_name(
+                            pattern.match(row["name"]).group())
+            row["surname"] = format_complex_name(
+                            pattern.match(row["surname"]).group())
+
+        # Processing emails
+        for row in resFile:
+            if parseaddr(row["email"])[1] != "":
+                row["email"] = parseaddr(row["email"])[1].lower()
+                row["is_valid"] = True
+            else:
+                row["email"] = row["email"].lower()
+                row["is_valid"] = False
